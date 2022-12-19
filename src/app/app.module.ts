0848234f3385a67982c5791f7072d89b1896cc24
Perju Gatar <yohanam.paulus@gmail.com>
2022-12-19 22:27:40 +0100

    añade angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { ObsRoutingModule } from './app-routing.module';
 import { ObsComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ObsComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ObsRoutingModule
+    ObsRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [],
