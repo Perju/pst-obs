e08b68d31644b394939f0f7c6454f33ed0986e27
Perju Gatar <yohanam.paulus@gmail.com>
2022-12-20 18:23:55 +0100

    Generar estructura básica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { ObsRoutingModule } from './app-routing.module';
-import { ObsComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OnboardingComponent } from './views/onboarding/onboarding.component';
-import { HeaderComponent } from './core/header/header.component';
-import { LeftSideBarComponent } from './core/left-side-bar/left-side-bar.component';
-import { RightSideBarComponent } from './core/right-side-bar/right-side-bar.component';
-import { FooterComponent } from './core/footer/footer.component';
+import { ObsRoutingModule } from './app-routing.module'
+import { ObsComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { OnboardingComponent } from './views/onboarding/onboarding.component'
+import { MatGridListModule } from '@angular/material/grid-list'
 
 @NgModule({
   declarations: [
     ObsComponent,
     OnboardingComponent,
-    HeaderComponent,
-    LeftSideBarComponent,
-    RightSideBarComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
     ObsRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [ObsComponent],
-  exports: [ObsComponent]})
-export class ObsModule { }
+  exports: [ObsComponent]
+})
+export class ObsModule {}
