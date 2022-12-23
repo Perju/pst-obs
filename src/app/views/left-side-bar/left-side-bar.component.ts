89bd686cafee7290fca097aa811a0002a7b7b5d0
Perju Gatar <yohanam.paulus@gmail.com>
2022-12-23 17:04:23 +0100

    Estilos generalres y de navegación

--- src/app/views/left-side-bar/left-side-bar.component.ts
+++ src/app/views/left-side-bar/left-side-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'obs-left-side-bar',
@@ -6,13 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-side-bar.component.sass']
 })
 export class LeftSideBarComponent {
-  public obsLinks: { label: string; href: string }[]
-  constructor() {
+  public obsLinks: { label: string; href: string; class?: string }[]
+  constructor(private router: Router) {
     this.obsLinks = [
       { label: 'Conectar', href: '' },
       { label: 'Controlador', href: 'controller' },
       { label: 'Ayuda', href: 'help' }
     ]
   }
-  ngOnInit() {}
+
+  ngOnInit() {
+    this.markCurrentPosition()
+  }
+  markCurrentPosition() {
+    console.log(this.router.url)
+    this.obsLinks = this.obsLinks.map((e) => {
+      e.class = '/' + e.href === this.router.url ? 'current-position' : ''
+      return e
+    })
+  }
+  navigate(href: string) {
+    this.router.navigate([href]).then(()=>this.markCurrentPosition())
+  }
 }
