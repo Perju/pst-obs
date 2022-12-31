4c64f44de5774779d27bbbf68115f7b5d9e8faea
Perju Gatar <yohanam.paulus@gmail.com>
2022-12-31 22:03:21 +0100

    Añadir sliders, mejorar estilos y fix typo

--- src/app/models/source.model.ts
+++ src/app/models/source.model.ts
@@ -1,7 +1,7 @@
 import { TransformData } from './transform-data.model'
 
 export interface Source {
-  sceneItemEnables: boolean
+  sceneItemEnabled: boolean
   sourceName: string
   sceneItemTransform: TransformData
 }
