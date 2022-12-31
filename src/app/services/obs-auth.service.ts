6234be81b90c018210441b8a8b038bb3a797bd98
Perju Gatar <yohanam.paulus@gmail.com>
2023-01-01 00:14:53 +0100

    Lógica de login y navegación

--- src/app/services/obs-auth.service.ts
+++ src/app/services/obs-auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import OBSWebSocket from 'obs-websocket-js'
+import { Router } from '@angular/router'
 import { BehaviorSubject, tap } from 'rxjs'
 import { ObsApiService, ObsUrl } from './obs-api.service'
 
@@ -10,13 +10,29 @@ export class ObsAuthService {
   private _isLoggedIn$ = new BehaviorSubject<boolean>(false)
   isLoggedIn$ = this._isLoggedIn$.asObservable()
 
-  constructor(private obsApi: ObsApiService) {}
+  constructor(private obsApi: ObsApiService, private router: Router) {}
 
   public login(obsUrl: ObsUrl, password: string) {
     return this.obsApi.login(obsUrl, password).pipe(
       tap((response: any) => {
         this.obsApi.onConnectionClosed().subscribe({
-          next: () => this._isLoggedIn$.next(false)
+          next: () => {
+            const url = this.router.url
+            if (
+              url.startsWith('/obs') &&
+              url !== '/obs/login' &&
+              url !== '/obs/help'
+            ) {
+              console.log(
+                url,
+                url.startsWith('/obs') &&
+                  url !== '/obs/login' &&
+                  url !== '/obs/help'
+              )
+              this.router.navigateByUrl('/obs/login')
+            }
+            return this._isLoggedIn$.next(false)
+          }
         })
         localStorage.setItem('obsAuth', response)
         this._isLoggedIn$.next(true)
