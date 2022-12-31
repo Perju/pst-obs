a3079f3c38d1cbbee24b30be07b221b703463255
Perju Gatar <yohanam.paulus@gmail.com>
2022-12-31 21:33:03 +0100

    Mover la autentificación a un servicio de guarda

--- src/app/modules/obs-routing.module.ts
+++ src/app/modules/obs-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { HelpComponent } from '../views/help/help.component'
 import { LoginComponent } from '../views/login/login.component'
 import { ControllerComponent } from '../views/controller/controller.component'
+import { LoggedInGuardService } from '../services/logged-in-guard.service'
 
 export const obsRoutes: Routes = [
   {
     path: 'obs',
     children: [
       { path: 'help', component: HelpComponent },
-      { path: 'controller', component: ControllerComponent },
+      { path: 'controller', component: ControllerComponent, canActivate: [LoggedInGuardService] },
       { path: 'login', component: LoginComponent }
     ]
   }
--- src/app/views/controller/controller.component.ts
+++ src/app/views/controller/controller.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import { ObsAuthService } from 'src/app/services/obs-auth.service'
-import { Router } from '@angular/router'
-import { Location } from '@angular/common'
 
 import { Scene } from '../../models/scene.model'
+
 import { ObsApiService } from 'src/app/services/obs-api.service'
 
 @Component({
@@ -13,61 +11,35 @@ import { ObsApiService } from 'src/app/services/obs-api.service'
 })
 export class ControllerComponent implements OnInit {
   public scenes: Scene[] = []
-  constructor(
-    private obsApi: ObsApiService,
-    private obsAuth: ObsAuthService,
-    private location: Location
-  ) {
+  public currentScene: Scene = { sceneName: 'Inicio', sceneIndex: 0 }
+  constructor(private obsApi: ObsApiService) {
     this.scenes = [
       { sceneName: 'Inicio', sceneIndex: 0 },
       { sceneName: 'Jugando', sceneIndex: 1 },
       { sceneName: 'Charlando', sceneIndex: 2 }
     ]
   }
-  ngOnInit() {
-    this.obsAuth.isLoggedIn$.subscribe({
-      next: (data) => {
-        if (data) console.log('Controller component: ', data)
-        else this.location.back()
-      },
-      error: (error) => {
-        console.log(error)
-      },
-      complete: () => {
-        console.log('Completed')
-      }
-    })
 
+  ngOnInit() {
     // obtener lista de escenas
     this.obsApi.getScenes().subscribe({
-      next: (data) => {
-        console.log('GetScenes: ', data.scenes)
-        this.scenes = data.scenes
-      },
-      error: (error) => {
-        console.log(error)
-      },
+      next: (data) => (this.scenes = data.scenes),
+      error: (error) => console.log(error),
       complete: () => {
-        //obtener lista de fuentes de cada escena
-        this._getSources()
+        this.scenes.forEach((scene) => {
+          this._getSources(scene)
+        })
       }
-    })
+    });
+    (valor: any)=>{return valor}
   }
 
   //obtener lista de fuentes de cada escena
-  private _getSources() {
-    this.scenes.forEach((scene) => {
-      console.log()
-      this.obsApi.getSources(scene.sceneName).subscribe({
-        next: (data) => {
-          scene.sceneItems = data.sceneItems
-          console.log("GetSources:", scene.sceneItems)
-        },
-        error: (error) => {
-          console.log(error)
-        },
-        complete: () => {}
-      })
+  private _getSources(scene: Scene) {
+    this.obsApi.getSources(scene.sceneName).subscribe({
+      next: (data) => (scene.sceneItems = data.sceneItems),
+      error: (error) => console.log(error),
+      complete: () => {}
     })
   }
 }
